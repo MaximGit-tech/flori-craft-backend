6694405c18ce5557a365585563a2f47b075db5de
MaximGit-tech <mykondratyev@gmail.com>
2026-01-06 18:22:14 +0300

    fix: fix settings

--- FloriCraft/settings.py
+++ FloriCraft/settings.py
@@ -86,10 +86,10 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': dj_database_url.parse(
         os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
-
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
