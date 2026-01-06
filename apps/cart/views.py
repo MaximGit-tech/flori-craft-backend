2f90441d5ad72e4f8f5f23323b0b6649f1e70881
MaximGit-tech <mykondratyev@gmail.com>
2026-01-07 02:18:02 +0300

    feature: add CartItemView

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -19,3 +19,42 @@ class CartView(APIView):
             'items': serializer.data
         })
 
+
+class CartItemView(APIView):
+    def post(self, request):
+        if not request.user:
+            return Response(
+                {'error': 'unauthorized'},
+                status=401
+            )
+
+        product_id = request.data.get('product_id')
+        if not product_id:
+            return Response(
+                {'error': 'product_id required'},
+                status=400
+            )
+
+        from .services.db_cart import add_item
+        add_item(request.user, product_id)
+
+        return Response({'status': 'ok'})
+
+    def delete(self, request):
+        if not request.user:
+            return Response(
+                {'error': 'unauthorized'},
+                status=401
+            )
+
+        product_id = request.data.get('product_id')
+        if not product_id:
+            return Response(
+                {'error': 'product_id required'},
+                status=400
+            )
+
+        from .services.db_cart import remove_item
+        remove_item(request.user, product_id)
+
+        return Response({'status': 'ok'})
