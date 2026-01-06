f589ec27aa9c256625c1622f835cc164b92c1957
MaximGit-tech <mykondratyev@gmail.com>
2026-01-06 22:12:05 +0300

    style: changed urls

--- apps/custom_auth/urls.py
+++ apps/custom_auth/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import SendSmsView, VerifySmsRegisterView, LogoutView, ProfileView, VerifySmsLoginView
 
 urlpatterns = [
-    path("send-sms/", SendSmsView.as_view()),
+    path("send_sms/", SendSmsView.as_view()),
     path("verify_register/", VerifySmsRegisterView.as_view()),
-    path("verify_login", VerifySmsLoginView.as_view()),
+    path("verify_login/", VerifySmsLoginView.as_view()),
     path("logout/", LogoutView.as_view()),
     path("profile/", ProfileView.as_view())
 ]
