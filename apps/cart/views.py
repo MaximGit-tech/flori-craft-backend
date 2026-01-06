66d266d085953dd91383fe14c41dfa3e434aac93
MaximGit-tech <mykondratyev@gmail.com>
2026-01-06 22:54:59 +0300

    features: add CartView

---
+++ apps/cart/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class CartView(APIView):
+    def get(self, request):
+        if not request.user:
+            return Response(
+                {'error': 'unauthorized'},
+                status=401
+            )
+
+        from .services.db_cart import get_items
+        return Response({
+            'items': get_items(request.user)
+        })
