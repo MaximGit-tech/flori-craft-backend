73a5f559fb01694e02ac41a9100175cc2fc8ec50
MaximGit-tech <mykondratyev@gmail.com>
2026-02-02 21:29:04 +0300

    quick fix

--- FloriCraft/settings.py
+++ FloriCraft/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'apps.custom_auth.middleware.CookieUserMiddleware',
+    # 'apps.custom_auth.middleware.CookieUserMiddleware',  # Отключен - DRF использует authentication classes
 ]
 
 CORS_ALLOWED_ORIGINS = [
