c9837c9b131748e2276431b190b9221164726545
MaximGit-tech <mykondratyev@gmail.com>
2026-02-05 22:25:33 +0300

    fix: fix cors allowed origins

--- FloriCraft/settings.py
+++ FloriCraft/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://flori-craft-front.vercel.app",
+    "https://flori-craft.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
