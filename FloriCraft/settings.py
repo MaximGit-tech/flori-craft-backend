be1178f27c344de3254ea119bdae8533fdbb54fa
MaximGit-tech <mykondratyev@gmail.com>
2026-02-07 22:52:48 +0300

    fix: корсы пофиксил

--- FloriCraft/settings.py
+++ FloriCraft/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://flori-craft-front.vercel.app"
+    "https://flori-craft.vercel.app/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
